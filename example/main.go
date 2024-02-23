360f97f418d6b21a48edc54852a1f7fc7adf9eb0
Liam Aikin <liamaikin@gmail.com>
2024-02-23 22:32:37 +1100

    added create, update and delete for users

--- example/main.go
+++ example/main.go
@@ -24,7 +24,7 @@ func main() {
 	user := pinotModel.User{
 		Username:  "liam1",
 		Password:  "password",
-		Component: "Broker",
+		Component: "BROKER",
 		Role:      "admin",
 	}
 
@@ -33,11 +33,27 @@ func main() {
 		log.Panic(err)
 	}
 
-	_, err = client.CreateUser(userBytes)
+	updateUser := pinotModel.User{
+		Username:  "liam1",
+		Password:  "password",
+		Component: "BROKER",
+		Role:      "user",
+	}
+
+	updateUserBytes, err := json.Marshal(user)
 	if err != nil {
 		log.Panic(err)
 	}
 
+	// Create User
+	createResp, err := client.CreateUser(userBytes)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(createResp.Status)
+
+	// Read Users
 	userResp, err := client.GetUsers()
 	if err != nil {
 		log.Panic(err)
@@ -46,5 +62,20 @@ func main() {
 	for userName, info := range userResp.Users {
 		fmt.Println(userName, info)
 	}
+
+	// Update User
+	updateResp, err := client.UpdateUser(updateUser.Username, updateUser.Component, false, updateUserBytes)
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println(updateResp.Status)
+
+	// Delete User
+	delResp, err := client.DeleteUser(user.Username, user.Component)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println(delResp.Status)
 
 }
